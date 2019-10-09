b7ee37c165adb65e40462a5d503952b5f55fe60e
Nádia Nami Abe <abe.nami0510@gmail.com>
2019-10-09 09:25:26 -0300

    routes and controllers done

--- src/app.js
+++ src/app.js
@@ -1,16 +1,24 @@
 'use strict'
 
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
 const router = express.Router();
 
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node Store API",
-        version: '0.0.1'
-    });
-});
-app.use('/', route);
+//carrega as rotas 
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+
+
+
+app.use('/', indexRoute);
+app.use('/products', productRoute);
+
+
 
 module.exports = app;
